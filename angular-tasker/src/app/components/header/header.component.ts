5cab4f23e31a8f043bc39e1cd1594557d2651a46
Sebastian F <sebpfave2@gmail.com>
2021-08-20 23:04:31 -0400

    Toggle task form

--- angular-tasker/src/app/components/header/header.component.ts
+++ angular-tasker/src/app/components/header/header.component.ts
@@ -1,19 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
   title: string = 'Task Tracker';
+  showAddTask: boolean = false;
+  subscription?: Subscription;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService.onToggle().subscribe((value) => {
+      this.showAddTask = value;
+    });
   }
 
-  toggleAddTask(){
-    console.log('toggle');
+  ngOnInit(): void {}
+
+  toggleAddTask() {
+    this.uiService.toggleAddTask();
   }
 }
